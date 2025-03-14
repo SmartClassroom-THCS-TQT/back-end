72929e61e6a5a59c4394b27a0f87d32f9a67babd
CodeBotX <minhtien.etbk@gmail.com>
2025-03-14 22:23:23 +0700

    return role

--- users/views.py
+++ users/views.py
@@ -47,8 +47,10 @@ class ApiLoginView(APIView):
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
+            role = user.role
             return Response({
                 "message": "Đăng nhập thành công",
+                "role": role,
                 "access_token": access_token,  
                 "refresh_token": refresh_token  
             }, status=status.HTTP_200_OK)
